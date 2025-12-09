9a09509f0407c2ff1bfbb64c7fb2a91ecaf756a2
Sam7AR <samazuaje2k4w@gmail.com>
2025-12-08 20:24:04 -0400

    create, alter e inserts de productos relacionados

--- macro-script.sql
+++ macro-script.sql
@@ -186,6 +186,14 @@ CREATE TABLE CATALOGOS (
         PRIMARY KEY (id_pais, id_juguete)
 );
 
+CREATE TABLE prods_relacionados (
+    id_jgt_base NUMBER(7) NOT NULL,
+    id_jgt_rela NUMBER(7) NOT NULL,
+
+    CONSTRAINT pk_prods_rela
+        PRIMARY KEY (id_jgt_base, id_jgt_rela)
+);
+
 --ALTERS
 ALTER TABLE clientes
   ADD(
@@ -298,6 +306,18 @@ ADD (
         FOREIGN KEY (id_juguete)
         REFERENCES JUGUETES(id)
 );
+
+ALTER TABLE prods_relacionados
+ADD (
+    CONSTRAINT fk_prods_relacionados_jgt_base
+        FOREIGN KEY (id_jgt_base)
+        REFERENCES JUGUETES(id),
+        
+    CONSTRAINT fk_prods_relacionados_jgt_rela
+        FOREIGN KEY (id_jgt_rela)
+        REFERENCES JUGUETES(id)
+);
+
 --FUNCIONES
 CREATE OR REPLACE FUNCTION edad(fec_naci DATE) RETURN NUMBER IS
     BEGIN
@@ -737,3 +757,20 @@ INSERT INTO CATALOGOS (id_pais, id_juguete, lim_compra_ol) VALUES (3, 7, 8);  --
 INSERT INTO CATALOGOS (id_pais, id_juguete, lim_compra_ol) VALUES (4, 2, 4);  -- Hogwarts Castle (4 unid.)
 INSERT INTO CATALOGOS (id_pais, id_juguete, lim_compra_ol) VALUES (4, 3, 3);  -- Nether Fortress (3 unid.)
 INSERT INTO CATALOGOS (id_pais, id_juguete, lim_compra_ol) VALUES (4, 5, 15); -- Dementor (15 unid.)
+
+-- PRODUCTOS RELACIONADOS
+-- Star Wars: Millennium Falcon ↔ X-Wing (mismo universo)
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (1, 4);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (4, 1);
+
+-- Harry Potter: Hogwarts Castle ↔ Dementor Figure (mismo castillo)
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (2, 5);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (5, 2);
+
+-- Minecraft Nether: Fortress ↔ Zombie Pigman ↔ Micro Creeper
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (3, 6);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (6, 3);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (3, 7);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (7, 3);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (6, 7);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (7, 6);
--- producto_relacionado/create_prod_rela.sql
+++ producto_relacionado/create_prod_rela.sql
@@ -1,15 +1,34 @@
 CREATE TABLE prods_relacionados (
-    id_jgt_base NUMBER NOT NULL,
-    id_jgt_rela NUMBER NOT NULL,
+    id_jgt_base NUMBER(7) NOT NULL,
+    id_jgt_rela NUMBER(7) NOT NULL,
 
     CONSTRAINT pk_prods_rela
-        PRIMARY KEY (id_jgt_base, id_jgt_rela),
+        PRIMARY KEY (id_jgt_base, id_jgt_rela)
+);
 
+ALTER TABLE prods_relacionados
+ADD (
     CONSTRAINT fk_prods_relacionados_jgt_base
         FOREIGN KEY (id_jgt_base)
-        REFERENCES juguetes(id),
-
+        REFERENCES JUGUETES(id),
+        
     CONSTRAINT fk_prods_relacionados_jgt_rela
         FOREIGN KEY (id_jgt_rela)
-        REFERENCES juguetes(id)
-)
+        REFERENCES JUGUETES(id)
+);
+
+-- Star Wars: Millennium Falcon ↔ X-Wing (mismo universo)
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (1, 4);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (4, 1);
+
+-- Harry Potter: Hogwarts Castle ↔ Dementor Figure (mismo castillo)
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (2, 5);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (5, 2);
+
+-- Minecraft Nether: Fortress ↔ Zombie Pigman ↔ Micro Creeper
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (3, 6);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (6, 3);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (3, 7);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (7, 3);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (6, 7);
+INSERT INTO prods_relacionados (id_jgt_base, id_jgt_rela) VALUES (7, 6);
